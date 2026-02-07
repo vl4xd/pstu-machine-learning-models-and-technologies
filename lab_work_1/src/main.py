9a773bcf698b7f45d910698672cd449c93826966
Platonov Vladislav <vlaxddd@yandex.ru>
2026-02-07 17:57:14 +0500

    update and add

--- lab_work_1/src/main.py
+++ lab_work_1/src/main.py
@@ -1,13 +1,37 @@
 import sys
 from PyQt5 import QtWidgets
 
-from lab_work_1.src.app.main_window import Ui_MainWindow
+from app.main_window import Ui_MainWindow
+
+class MyApp(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyApp, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        # self.ui.radioButton.clicked.connect()
+        # self.ui.radioButton_2.clicked.connect()
+        self.ui.pushButton.clicked.connect(self.get_predict)
+
+    def on_model_changed(self):
+        """Обработчик изменения выбора модели"""
+        # if self.ui.radioButton.
+        pass
+
+    def hide_wind_speed_fields(self):
+        """Скрыть поля скорости ветра"""
+        pass
+
+    def show_wind_speed_fields(self):
+        """Показать поля скорости ветра"""
+        pass
+
+    def get_predict(self):
+        pass
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    app = QtWidgets.QApplication([])
+    application = MyApp()
+    application.show()
     sys.exit(app.exec_())
---
+++ lab_work_1/src/temp.py
@@ -0,0 +1,4 @@
+from pipeline.model_1 import Model1
+
+m = Model1()
+m.predict(1, 1)
